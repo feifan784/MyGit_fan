c55dbd9abe1cc2d8a784290bfd76392fdd57f0ff
feifan784 <x_feifan1070322018@163.com>
2016-11-10 11:14:17 +0800

    完善了数据模型类

--- app/src/main/java/com/example/xu/mygit_fan/MyBean.java
+++ app/src/main/java/com/example/xu/mygit_fan/MyBean.java
@@ -5,4 +5,25 @@ package com.example.xu.mygit_fan;
  */
 public class MyBean {
     private String name;
+    private int age;
+
+    public MyBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
